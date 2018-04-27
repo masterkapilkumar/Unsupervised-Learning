90c8b794573255d44d5dde31847ce5d8ff45d3a0
Kapil Kumar <masterkapilkumar@gmail.com>
2018-04-27 22:48:09 +0530

    Neural Networks code complete.

--- Q1/Unsupervised.py
+++ Q1/Unsupervised.py
@@ -1,14 +1,20 @@
-import numpy as np
-import sys
-import os
 from collections import defaultdict
 import matplotlib.pyplot as plt
-from sklearn import svm
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
+import numpy as np
 import pickle
+import sys
+import os
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
+from sklearn import svm
+
+from sklearn.cluster import KMeans
+
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.models import Sequential
+from keras.utils import np_utils
 
 from timeit import default_timer as timer
 
@@ -39,19 +45,12 @@ def calculate_time_elapsed():
     start = timer()
     return time_elapsed
 
-def save_submission_file(data, pred_file):
-    f = open(pred_file,'w')
-    f.write("ID,CATEGORY\n")
-    for i,pred in enumerate(data):
-        f.write(str(i)+","+str(pred)+'\n')
-    f.close()
-
 def Q1(train_x, train_y, test_data, model=None, pred_file="predictions.txt"):
-    x = train_x.reshape((train_x.shape[0]*train_x.shape[1],train_x.shape[2]))
+    
     if(not model):
         ##########TRAINING##############
         kmeans = KMeans(n_clusters=20, n_init=10)
-        kmeans.fit(x)
+        kmeans.fit(train_x)
         pickle.dump(kmeans, open("kmeans_model", 'wb'))
         model = kmeans
         
@@ -99,7 +98,6 @@ def Q1(train_x, train_y, test_data, model=None, pred_file="predictions.txt"):
     
     
 def Q2(train_x, train_y, test_data, model=None, pred_file="predictions.txt"):
-    x = train_x.reshape((train_x.shape[0]*train_x.shape[1],train_x.shape[2]))
     
 # =============================================================================
 #     #make validation data set
@@ -109,8 +107,8 @@ def Q2(train_x, train_y, test_data, model=None, pred_file="predictions.txt"):
 # =============================================================================
     
     pca = PCA(n_components=50)
-    x_projected = pca.fit_transform(x)/255.0
-    test_projected = pca.fit_transform(test_data)/255.0
+    x_projected = scale(pca.fit_transform(train_x))
+    test_projected = scale(pca.transform(test_data))
     
     print("PCA done...")
     print("Time taken: %.2fs\n"%(calculate_time_elapsed()))
@@ -149,7 +147,7 @@ def Q2(train_x, train_y, test_data, model=None, pred_file="predictions.txt"):
     correct = 0
     for (corr,pred) in zip(train_y, predictions):
         if(corr==pred): correct += 1
-    accuracy = correct/x.shape[0] * 100
+    accuracy = correct/train_x.shape[0] * 100
     print("Training Accuracy: %.2f%%" %(accuracy) )
     print("Libsvm train set prediction complete...")
     print("Time taken: %.2fs\n"%(calculate_time_elapsed()))
@@ -161,9 +159,79 @@ def Q2(train_x, train_y, test_data, model=None, pred_file="predictions.txt"):
     
     print("Libsvm test set prediction complete...")
     print("Time taken: %.2fs\n"%(calculate_time_elapsed()))
+
+def Q3(train_x, train_y, test_data, valid_x=None, valid_y=None, model=None, pred_file="predictions.txt"):
     
+    train_x = scale(train_x)
+    valid_x = scale(valid_x)
+    test_data = scale(test_data)
+    
+    model = Sequential()
+    model.add(Dense(784, activation='sigmoid', input_shape=(train_x.shape[1],)))
+    model.add(Dense(20, activation='softmax'))
+    
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    train_labels_one_hot = np_utils.to_categorical(train_y)
+    valid_labels_one_hot = np_utils.to_categorical(valid_y)
+    model.fit(train_x, train_labels_one_hot, batch_size=100, epochs=15, verbose=1, validation_data=(valid_x, valid_labels_one_hot))
+#    model.fit(train_x, train_labels_one_hot, batch_size=100, epochs=10, verbose=1)
+    predictions = model.predict(test_data, verbose=1)
+    predictions = np.argmax(predictions, axis=1)
+    predictions = np.array(list(map(lambda a:labels_map[a], predictions)))
+    save_submission_file(predictions, pred_file)
+    return predictions
+
+def Q4(train_x, train_y, test_data, valid_x=None, valid_y=None, model=None, pred_file="predictions.txt"):
+    
+    train_x = scale(train_x).reshape(train_x.shape[0],28,28,1)
+    valid_x = scale(valid_x).reshape(valid_x.shape[0],28,28,1)
+    test_data = scale(test_data).reshape(test_data.shape[0],28,28,1)
+    
+    model = Sequential()
+    ## 1 CNN layer
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(28,28,1)))
+    # model.add(Conv2D(32, (3, 3), activation='relu'))
+    ## Max Pooling
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    ## 1 fully connected layer
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(20, activation='softmax'))
+    
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    train_labels_one_hot = np_utils.to_categorical(train_y)
+    valid_labels_one_hot = np_utils.to_categorical(valid_y)
+    model.fit(train_x, train_labels_one_hot, batch_size=100, epochs=15, verbose=1, validation_data=(valid_x, valid_labels_one_hot))
+#    model.fit(train_x, train_labels_one_hot, batch_size=100, epochs=10, verbose=1)
+    predictions = model.predict(test_data, verbose=1)
+    predictions = np.argmax(predictions, axis=1)
+    predictions = np.array(list(map(lambda a:labels_map[a], predictions)))
+    save_submission_file(predictions, pred_file)
+    return predictions
+
+
+
+def make_validation_set(data_x, data_y, n, rand_file):
+    indices = np.load(rand_file)
+    train_x = np.array([data_x[indices[i]] for i in range(data_x.shape[0]-n)])
+    train_y = data_y[indices[:-n]]
+    valid_x = np.array([data_x[indices[i]] for i in range(data_x.shape[0]-n,data_x.shape[0])])
+    valid_y = data_y[indices[-n:]]
+    return train_x, train_y, valid_x, valid_y
+    
+
 def predict_from_model(model, data):
     return model.predict(data)
+
+def save_submission_file(data, pred_file):
+    f = open(pred_file,'w')
+    f.write("ID,CATEGORY\n")
+    for i,pred in enumerate(data):
+        f.write(str(i)+","+str(pred)+'\n')
+    f.close()
     
 if __name__=='__main__':
     
@@ -177,11 +245,21 @@ if __name__=='__main__':
         train_x, train_y, labels_map = load_train_data("train/")
         test_data = load_test_data("test.npy")
     
+    train_x = train_x.reshape((train_x.shape[0]*train_x.shape[1],train_x.shape[2]))
+    valid_size = 10000
+    train_x, train_y, valid_x, valid_y = make_validation_set(train_x, train_y, valid_size, "rand_indices.npy")
+    
     print("Reading data complete...")
     print("Time taken: %.2fs\n"%(calculate_time_elapsed()))
     
-    aa=Q1(train_x, train_y, test_data, "kmeans_model")
+    #Q1(train_x, train_y, test_data, "kmeans_model")
+    
+
     
     #visualize_vector(test_data[1])
-    #Q2(train_x, train_y, test_data)             #TODO: apply PCA then scale data
+    #Q2(train_x, train_y, test_data)
+    
+    #Q3(train_x, train_y, test_data, valid_x, valid_y)
+    
+    #Q4(train_x, train_y, test_data, valid_x, valid_y)
     
